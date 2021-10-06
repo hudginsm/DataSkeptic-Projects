883ba44157d732223cd00e13447b62f6d85a0390
Marcus Adison Hudgins <hudginsm@users.noreply.github.com>
2021-10-05 23:56:49 -0500

    yep

--- C#/C# Programming in easy steps/Hello.cs
+++ C#/C# Programming in easy steps/Hello.cs
@@ -7,6 +7,8 @@ namespace Hello {
     static void Main (string[] args) {
 
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Please press any key to continue...");
+      Console.ReadKey();
     }
   }
 }
